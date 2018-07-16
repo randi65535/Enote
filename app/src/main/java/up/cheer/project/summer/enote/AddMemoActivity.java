2616967ab7a62a9d04cd6a5b33432a819244fba4
Hyun <sweetlulu486@gmail.com>
2018-07-17 00:18:43 +0900

    메모 등록 화면 디자인 업로드
    작성일자 2018-07-17

---
+++ app/src/main/java/up/cheer/project/summer/enote/AddMemoActivity.java
@@ -0,0 +1,14 @@
+package up.cheer.project.summer.enote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class AddMemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
