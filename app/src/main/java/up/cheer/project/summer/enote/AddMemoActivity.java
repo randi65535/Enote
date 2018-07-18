484ec2d8da15524a9aea0119051fc77185fdbc65
Hyun <sweetlulu486@gmail.com>
2018-07-18 18:23:40 +0900

    0718수 작업
    Isbn read가능
    추후 해야할 작업 : JSON Data 읽어들이기

--- app/src/main/java/up/cheer/project/summer/enote/AddMemoActivity.java
+++ app/src/main/java/up/cheer/project/summer/enote/AddMemoActivity.java
@@ -1,18 +1,74 @@
 package up.cheer.project.summer.enote;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 
 public class AddMemoActivity extends AppCompatActivity {
 
     Button insertMemoBtn, returnMenuBtn, searchIsbnBtn;
     EditText inputIsbnTextEdit, inputAuthorTextEdit, inputTitleTextEdit;
+    TextView isbnTextView;
+
+    Handler mHandler = new Handler();
+    String urlStr = "http://seoji.nl.go.kr/landingPage/SearchApi.do?cert_key=5e5a4c575334ac512cc7907e5020563e&result_style=json&page_no=1&page_size=10&isbn=9788992555784";
+
+    class mThread extends Thread{
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(urlStr);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    readStream(in);
+                    urlConnection.disconnect();
+                }else{
+                    Toast.makeText(getApplicationContext(), "에러발생", Toast.LENGTH_SHORT).show();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public void readStream(InputStream in){
+            final String data = readData(in);
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    isbnTextView.setText(data);
+                }
+            });
+        }
+
+        public String readData(InputStream is){
+            String data = "";
+            Scanner s = new Scanner(is);
+            while(s.hasNext()) data += s.nextLine() + "\n";
+            s.close();
+            return data;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +85,8 @@ public class AddMemoActivity extends AppCompatActivity {
         inputTitleTextEdit = findViewById(R.id.InputTitleTextEdit);
         inputAuthorTextEdit = findViewById(R.id.InputAuthorTextEdit);
 
+        isbnTextView = findViewById(R.id.IsbnTextView);
+
 
         searchIsbnBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +98,7 @@ public class AddMemoActivity extends AppCompatActivity {
 
                 String isbn = inputIsbnTextEdit.getText().toString().replace("-", "");
 
-//                Toast.makeText(getApplicationContext(), isbn, Toast.LENGTH_SHORT).show();
+                new mThread().start();
 
             }
         });
@@ -63,6 +121,8 @@ public class AddMemoActivity extends AppCompatActivity {
                     Toast.makeText(AddMemoActivity.this, "작가명을 기입하지 않았습니다.", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                new mThread().start();
             }
         });
 
@@ -70,10 +130,10 @@ public class AddMemoActivity extends AppCompatActivity {
         returnMenuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 finish();
             }
         });
 
     }
+
 }
--- app/src/main/java/up/cheer/project/summer/enote/MainActivity.java
+++ app/src/main/java/up/cheer/project/summer/enote/MainActivity.java
@@ -1,14 +1,30 @@
 package up.cheer.project.summer.enote;
 
 import android.content.Intent;
+import android.os.Debug;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     Button addMemoBtn, manageMemoBtn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         addMemoBtn = findViewById(R.id.AddMemoBtn);
         manageMemoBtn = findViewById(R.id.ManageMemoBtn);
+        tv = findViewById(R.id.TestText);
 
         addMemoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,10 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
-
-
-
-
-
 }
